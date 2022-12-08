12e47596a342eab93ddb026ee42a4e737c91cd4d
yung-coder <chandel.sanjeev5@gmail.com>
2022-12-08 22:45:45 +0530

    repair

--- flutter/notesapp/lib/main.dart
+++ flutter/notesapp/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
@@ -18,9 +22,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late final TextEditingController = _email;
+  late final TextEditingController _email;
 
-  late final TextEditingController = _password;
+  late final TextEditingController _password;
 
   @override
   void initState() {
@@ -41,16 +45,42 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Register')),
-      body: Column(
-        children: [
-          TextField(
-            controller: _email,
-          ),
-          TextField(
-            controller: _password,
-          ),
-          TextButton(onPressed: () async {}, child: const Text('Register')),
-        ],
+      body: FutureBuilder(
+        builder: (context, snapshot) => {
+          
+        },
+        child: Column(
+          children: [
+            TextField(
+              controller: _email,
+              enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(hintText: "Enter your email"),
+            ),
+            TextField(
+              controller: _password,
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration: const InputDecoration(hintText: "Enter your password"),
+            ),
+            TextButton(
+                onPressed: () async {
+                  await Firebase.initializeApp(
+                    options: DefaultFirebaseOptions.currentPlatform,
+                  );
+                  final email = _email.text;
+                  final password = _password.text;
+                  final userCreds = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                          email: email, password: password);
+      
+                  print(userCreds);
+                },
+                child: const Text('Register')),
+          ],
+        ),
       ),
     );
   }

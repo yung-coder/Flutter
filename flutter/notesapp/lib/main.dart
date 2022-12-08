2e7e2271848fd59626beff08ef7eef1a37c7f44d
yung-coder <chandel.sanjeev5@gmail.com>
2022-12-08 23:02:43 +0530

    firebase done

--- flutter/notesapp/lib/main.dart
+++ flutter/notesapp/lib/main.dart
@@ -50,34 +50,40 @@ class _HomePageState extends State<HomePage> {
           options: DefaultFirebaseOptions.currentPlatform,
         ),
         builder: (context, snapshot) {
-          return Column(children: [
-            TextField(
-              controller: _email,
-              enableSuggestions: false,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(hintText: "Enter your email"),
-            ),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration:
-                  const InputDecoration(hintText: "Enter your password"),
-            ),
-            TextButton(
-                onPressed: () async {
-                  final email = _email.text;
-                  final password = _password.text;
-                  final userCreds = await FirebaseAuth.instance
-                      .createUserWithEmailAndPassword(
-                          email: email, password: password);
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Column(children: [
+                TextField(
+                  controller: _email,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration:
+                      const InputDecoration(hintText: "Enter your email"),
+                ),
+                TextField(
+                  controller: _password,
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration:
+                      const InputDecoration(hintText: "Enter your password"),
+                ),
+                TextButton(
+                    onPressed: () async {
+                      final email = _email.text;
+                      final password = _password.text;
+                      final userCreds = await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: email, password: password);
 
-                  print(userCreds);
-                },
-                child: const Text('Register')),
-          ]);
+                      print(userCreds);
+                    },
+                    child: const Text('Register')),
+              ]);
+            default:
+              return const Text('Loading...');
+          }
         },
       ),
     );

972a29f140629808bd5378737f3c8bde3fae8fdc
yung-coder <chandel.sanjeev5@gmail.com>
2023-01-14 18:53:37 +0530

    ui done

--- todo_appp/lib/screens/home.dart
+++ todo_appp/lib/screens/home.dart
@@ -35,58 +35,115 @@ class Home extends StatelessWidget {
         backgroundColor: tdBGColor,
         elevation: 0,
       ),
-      body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Column(
+      body: Stack(children: [
+        Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(0),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: tdBlack,
+                        size: 20,
+                      ),
+                      prefixIconConstraints: BoxConstraints(
+                        maxHeight: 20,
+                        minWidth: 25,
+                      ),
+                      border: InputBorder.none,
+                      hintText: 'Search',
+                      hintStyle: TextStyle(color: tdGrey),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(
+                          top: 5,
+                          bottom: 20,
+                        ),
+                        child: const Text(
+                          'All todos',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      for (Todo todo in todoList)
+                        TodoItem(
+                          todo: todo,
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            )),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Row(
             children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                decoration: BoxDecoration(
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                    left: 20,
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 5,
+                  ),
+                  decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(0),
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: tdBlack,
-                      size: 20,
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0),
+                    ],
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Add a new todo',
+                      border: InputBorder.none,
                     ),
-                    prefixIconConstraints: BoxConstraints(
-                      maxHeight: 20,
-                      minWidth: 25,
-                    ),
-                    border: InputBorder.none,
-                    hintText: 'Search',
-                    hintStyle: TextStyle(color: tdGrey),
                   ),
                 ),
               ),
-              Expanded(
-                child: ListView(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(
-                        top: 5,
-                        bottom: 20,
-                      ),
-                      child: const Text(
-                        'All todos',
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                    for (Todo todo in todoList)
-                      TodoItem(
-                        todo: todo,
-                      ),
-                  ],
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+                ),
+                child: ElevatedButton(
+                  child: Text(
+                    '+',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: tdBlue,
+                    minimumSize: Size(60, 60),
+                    elevation: 10,
+                  ),
                 ),
               )
             ],
-          )),
+          ),
+        ),
+      ]),
     );
   }
 }

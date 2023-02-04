ab09a4d97ff3362b35a99ec14fd445d7a3dd781f
yung-coder <chandel.sanjeev5@gmail.com>
2023-02-04 16:17:32 +0530

    page

---
+++ cart/lib/pages/HomePage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final int days = 30;
+  final String name = "Checking";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days of flutter $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}

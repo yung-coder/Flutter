cbf1addf2c4162be319dcd2b2c6b80ee5cba96d6
yung-coder <chandel.sanjeev5@gmail.com>
2023-02-05 14:18:51 +0530

    json

--- cart/lib/pages/HomePage.dart
+++ cart/lib/pages/HomePage.dart
@@ -2,13 +2,36 @@ import 'package:cart/models/catalog.dart';
 import 'package:cart/widgets/drawer.dart';
 import 'package:cart/widgets/item_Widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Checking";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    final productsData = decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
     final dumyList = List.generate(10, (index) => CatalogModel.items[0]);

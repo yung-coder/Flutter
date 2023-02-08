e197e2d87e33482e6cac3f4ddfc8497294156ff5
yung-coder <chandel.sanjeev5@gmail.com>
2023-02-08 18:44:47 +0530

    routes

--- cart/lib/main.dart
+++ cart/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: MyThemes.lightTheme(context),
       debugShowCheckedModeBanner: false,
       darkTheme: MyThemes.darkTheme(context),
-      initialRoute: MyRoutes.HomeRoute,
+      initialRoute: MyRoutes.LoginRoute,
       routes: {
         MyRoutes.HomeRoute: (context) => HomePage(),
         MyRoutes.LoginRoute: (context) => LoginPage(),

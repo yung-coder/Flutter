87bfa334b2bd54eee79421de2569c15ef2ec648c
yung-coder <chandel.sanjeev5@gmail.com>
2023-02-19 02:33:59 +0530

    news api simpl api

--- newsapp/lib/Pages/HomePage.dart
+++ newsapp/lib/Pages/HomePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/api.dart';
+import 'package:newsapp/components/customtile.dart';
+import 'package:newsapp/model/Article.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,7 +14,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text(
+          "News App ",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: FutureBuilder(
+        builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
+          if (snapshot.hasData) {
+            //Now let's make a list of articles
+            List<Article>? articles = snapshot.data;
+            return ListView.builder(
+              //Now let's create our custom List tile
+              itemCount: articles!.length,
+              itemBuilder: (context, index) =>
+                  customListTile(articles[index], context),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }

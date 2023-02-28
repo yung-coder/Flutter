98dea4c7b40fb8fcc517810941a41ab647ab4029
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-02-28 14:11:23 +0530

    login signin

--- videocall/lib/screens/login_screen.dart
+++ videocall/lib/screens/login_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:videocall/widgets/custom_button.dart';
+import 'package:videocall/widgets/custom_textfiled.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String routeName = '/login';
@@ -10,8 +13,56 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login up'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: size.height * 0.1,
+              ),
+              const Text(
+                'Email',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: CustomTextFiled(controller: _emailController),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: CustomTextFiled(controller: _passwordController),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomButton(text: 'Login', onTap: () {})
+            ],
+          ),
+        ),
+      ),
+    );
+    ;
   }
 }

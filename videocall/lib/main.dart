c4c3c04de5bfae28471d1e1b0afa5f801110b680
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-02-28 16:35:57 +0530

    persist state

--- videocall/lib/main.dart
+++ videocall/lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:videocall/firebase/auth_methods.dart';
 import 'package:videocall/providers/user_provider.dart';
 import 'package:videocall/screens/home_screen.dart';
 import 'package:videocall/screens/login_screen.dart';
 import 'package:videocall/screens/onboarding_screen.dart';
 import 'package:videocall/screens/signup_screen.dart';
 import 'package:videocall/utils/colors.dart';
+import 'package:provider/provider.dart';
+import 'package:videocall/models/user.dart' as model;
+import 'package:videocall/widgets/loading_indicator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -42,7 +47,29 @@ class MyApp extends StatelessWidget {
         SignupScreen.routeName: (context) => const SignupScreen(),
         HomeScreen.routeName: (context) => const HomeScreen(),
       },
-      home: const OnboardingScreen(),
+      home: FutureBuilder(
+          future: AuthMethods()
+              .getCurrentUser(FirebaseAuth.instance.currentUser != null
+                  ? FirebaseAuth.instance.currentUser!.uid
+                  : null)
+              .then((value) {
+            if (value != null) {
+              Provider.of<UserProvider>(context, listen: false).setUser(
+                model.User.fromMap(value),
+              );
+            }
+            return value;
+          }),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const LoadingIndicator();
+            }
+
+            if (snapshot.hasData) {
+              return const HomeScreen();
+            }
+            return const OnboardingScreen();
+          }),
     );
   }
 }

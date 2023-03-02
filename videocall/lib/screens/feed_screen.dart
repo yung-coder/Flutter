c5c0a743f6563d975ab06eb4d49d4d6989813ccc
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-03 00:48:20 +0530

    feed

--- videocall/lib/screens/feed_screen.dart
+++ videocall/lib/screens/feed_screen.dart
@@ -1,5 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:videocall/firebase/fireStore_methods.dart';
+import 'package:videocall/models/livestream.dart';
+import 'package:videocall/responsive/responsive_layout.dart';
+import 'package:videocall/screens/broadcast.dart';
+import 'package:videocall/widgets/loading_indicator.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class FeedScreen extends StatefulWidget {
   const FeedScreen({super.key});
@@ -11,6 +19,174 @@ class FeedScreen extends StatefulWidget {
 class _FeedScreenState extends State<FeedScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10).copyWith(
+          top: 10,
+        ),
+        child: Column(
+          children: [
+            const Text(
+              'Live Users',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+            StreamBuilder<dynamic>(
+              stream: FirebaseFirestore.instance
+                  .collection('livestream')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const LoadingIndicator();
+                }
+
+                return Expanded(
+                  child: ResponsiveLatout(
+                    desktopBody: GridView.builder(
+                      itemCount: snapshot.data.docs.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                      ),
+                      itemBuilder: (context, index) {
+                        LiveStream post = LiveStream.fromMap(
+                            snapshot.data.docs[index].data());
+                        return InkWell(
+                          onTap: () async {
+                            await FireStoreMethods()
+                                .updateViewCount(post.channelId, true);
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => BroadCastScreen(
+                                  isBroadcaster: false,
+                                  channdelId: post.channelId,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 10,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: size.height * 0.35,
+                                  child: Image.network(
+                                    post.image,
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      post.username,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    Text(
+                                      post.title,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text('${post.viewers} watching'),
+                                    Text(
+                                      'Started ${timeago.format(post.startedAt.toDate())}',
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    mobileBody: ListView.builder(
+                        itemCount: snapshot.data.docs.length,
+                        itemBuilder: (context, index) {
+                          LiveStream post = LiveStream.fromMap(
+                              snapshot.data.docs[index].data());
+
+                          return InkWell(
+                            onTap: () async {
+                              await FireStoreMethods()
+                                  .updateViewCount(post.channelId, true);
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => BroadCastScreen(
+                                    isBroadcaster: false,
+                                    channdelId: post.channelId,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              height: size.height * 0.1,
+                              margin: const EdgeInsets.symmetric(vertical: 10),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  AspectRatio(
+                                    aspectRatio: 16 / 9,
+                                    child: Image.network(post.image),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        post.username,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                      Text(
+                                        post.title,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text('${post.viewers} watching'),
+                                      Text(
+                                        'Started ${timeago.format(post.startedAt.toDate())}',
+                                      ),
+                                    ],
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.more_vert,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

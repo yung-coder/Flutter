c3db974435e085401af82f2ba80d2185fd50fdd5
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-05 00:05:33 +0530

    socket

---
+++ game/lib/resources/socket_client.dart
@@ -0,0 +1,19 @@
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class SocketClient {
+  IO.Socket? socket;
+  static SocketClient? _instance;
+
+  SocketClient._internal() {
+    socket = IO.io('122.173.24.22/32:3000', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    socket!.connected;
+  }
+
+  static SocketClient? get instance {
+    _instance ??= SocketClient._internal();
+    return _instance;
+  }
+}

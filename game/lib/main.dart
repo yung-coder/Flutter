0d0089c348cf3ffc5f6e5584625741a1e9687200
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-04 19:48:41 +0530

    new app

---
+++ game/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:game/screens/main_menu_screen.dart';
+import 'package:game/utils/Colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+      ),
+      home: MainMenu(),
+    );
+  }
+}

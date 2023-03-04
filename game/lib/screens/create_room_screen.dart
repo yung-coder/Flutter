84569333de259efb93d468fa7891f411e98e42b6
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-04 23:21:57 +0530

    ui complete

--- game/lib/screens/create_room_screen.dart
+++ game/lib/screens/create_room_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:game/responsive/responsive.dart';
+import 'package:game/widgets/Custom_button.dart';
 import 'package:game/widgets/Custom_text_field.dart';
 import 'package:game/widgets/Custom_text.dart';
 
@@ -26,27 +28,34 @@ class _CreateRoomScreenState extends State<CreateRoomScreen> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const CustomText(
-              shadows: [
-                Shadow(
-                  blurRadius: 40,
-                  color: Colors.blue,
-                )
-              ],
-              text: 'Create Room',
-              fontSize: 70,
-            ),
-            SizedBox(
-              height: size.height * 0.08,
-            ),
-            CustomTextField(controller: _nameController, hintText: 'Enter name')
-          ],
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 40,
+                    color: Colors.blue,
+                  )
+                ],
+                text: 'Create Room',
+                fontSize: 70,
+              ),
+              SizedBox(
+                height: size.height * 0.08,
+              ),
+              CustomTextField(
+                  controller: _nameController, hintText: 'Enter name'),
+              SizedBox(
+                height: size.height * 0.08,
+              ),
+              CustomButton(onTap: () {}, text: 'Create')
+            ],
+          ),
         ),
       ),
     );
--- game/lib/screens/join_room_screen.dart
+++ game/lib/screens/join_room_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:game/responsive/responsive.dart';
+import 'package:game/widgets/Custom_button.dart';
+import 'package:game/widgets/Custom_text.dart';
+import 'package:game/widgets/Custom_text_field.dart';
 
 class JoinRoomScreen extends StatefulWidget {
   static String routeName = '/join-room';
@@ -11,8 +15,60 @@ class JoinRoomScreen extends StatefulWidget {
 }
 
 class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _gameidController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _nameController.dispose();
+    _gameidController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 40,
+                    color: Colors.blue,
+                  )
+                ],
+                text: 'Join Room',
+                fontSize: 70,
+              ),
+              SizedBox(
+                height: size.height * 0.08,
+              ),
+              CustomTextField(
+                controller: _nameController,
+                hintText: 'Enter name',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomTextField(
+                controller: _gameidController,
+                hintText: 'Enter game id',
+              ),
+              SizedBox(
+                height: size.height * 0.08,
+              ),
+              CustomButton(onTap: () {}, text: 'Join')
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

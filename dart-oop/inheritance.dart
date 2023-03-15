cf70f2247c718bba60a260c971a33c26ce6c1ee4
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-15 18:41:17 +0530

    inheritance

--- dart-oop/inheritance.dart
+++ dart-oop/inheritance.dart
@@ -1,36 +1,59 @@
-class Person {
-  // Properties
-  String? name;
-  int? age;
+// class Person {
+//   // Properties
+//   String? name;
+//   int? age;
 
-  // Method
-  void display() {
-    print("Name: $name");
-    print("Age: $age");
-  }
+//   // Method
+//   void display() {
+//     print("Name: $name");
+//     print("Age: $age");
+//   }
+// }
+
+// // Here In student class, we are extending the
+// // properties and methods of the Person class
+// class Student extends Person {
+//   // Fields
+//   String? schoolName;
+//   String? schoolAddress;
+
+//   // Method
+//   void displaySchoolInfo() {
+//     print("School Name: $schoolName");
+//     print("School Address: $schoolAddress");
+//   }
+// }
+
+// void main() {
+//   // Creating an object of the Student class
+//   var student = Student();
+//   student.name = "John";
+//   student.age = 20;
+//   student.schoolName = "ABC School";
+//   student.schoolAddress = "New York";
+//   student.display();
+//   student.displaySchoolInfo();
+// }
+
+class Person {
+  String name;
+  int age;
+
+  // Constructor
+  Person(this.name, this.age);
 }
 
-// Here In student class, we are extending the
-// properties and methods of the Person class
 class Student extends Person {
-  // Fields
-  String? schoolName;
-  String? schoolAddress;
-
-  // Method
-  void displaySchoolInfo() {
-    print("School Name: $schoolName");
-    print("School Address: $schoolAddress");
-  }
+  int rollNumber;
+  // super is used to refer parent class 
+  // Constructor
+  Student(String name, int age, this.rollNumber) : super(name, age);
 }
 
 void main() {
-  // Creating an object of the Student class
-  var student = Student();
-  student.name = "John";
-  student.age = 20;
-  student.schoolName = "ABC School";
-  student.schoolAddress = "New York";
-  student.display();
-  student.displaySchoolInfo();
+  var student = Student("John", 20, 1);
+  print("Student name: ${student.name}");
+  print("Student age: ${student.age}");
+  print("Student roll number: ${student.rollNumber}");
+
 }

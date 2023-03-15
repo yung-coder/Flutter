2ea7cbbf1890a49e9120835f83fcb07568badaf5
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-15 17:01:20 +0530

    constructors

--- dart-opp/dart-opp.dart
+++ dart-opp/dart-opp.dart
@@ -40,7 +40,32 @@ class Add {
   }
 }
 
+// void main() {
+//   Add a = Add.add3(10, 20 , 10);
+//   a.display();
+// }
+
+// Constant
+
+class Point {
+  final int x;
+  final int y;
+
+  const Point(this.x, this.y);
+}
+
 void main() {
-  Add a = Add.add3(10, 20 , 10);
-  a.display();
+  // p1 and p2 has the same hash code.
+  Point p1 = const Point(1, 2);
+  print("The p1 hash code is: ${p1.hashCode}");
+
+  Point p2 = const Point(1, 2);
+  print("The p2 hash code is: ${p2.hashCode}");
+  // without using const
+  // this has different hash code.
+  Point p3 = Point(2, 2);
+  print("The p3 hash code is: ${p3.hashCode}");
+
+  Point p4 = Point(2, 2);
+  print("The p4 hash code is: ${p4.hashCode}");
 }

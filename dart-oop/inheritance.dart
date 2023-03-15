aa02c9d1a707bfdc2b9579f0398cb5de6a5b5374
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-15 18:27:49 +0530

    folder update

---
+++ dart-oop/inheritance.dart
@@ -0,0 +1,36 @@
+class Person {
+  // Properties
+  String? name;
+  int? age;
+
+  // Method
+  void display() {
+    print("Name: $name");
+    print("Age: $age");
+  }
+}
+
+// Here In student class, we are extending the
+// properties and methods of the Person class
+class Student extends Person {
+  // Fields
+  String? schoolName;
+  String? schoolAddress;
+
+  // Method
+  void displaySchoolInfo() {
+    print("School Name: $schoolName");
+    print("School Address: $schoolAddress");
+  }
+}
+
+void main() {
+  // Creating an object of the Student class
+  var student = Student();
+  student.name = "John";
+  student.age = 20;
+  student.schoolName = "ABC School";
+  student.schoolAddress = "New York";
+  student.display();
+  student.displaySchoolInfo();
+}

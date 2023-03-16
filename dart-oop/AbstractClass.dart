cef503d9e8bbda27ffed56f5ac8e99d0011685a0
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-17 00:02:44 +0530

    abstract

---
+++ dart-oop/AbstractClass.dart
@@ -0,0 +1,93 @@
+abstract class Vehicle {
+  // Abstract method
+  void start();
+  // Abstract method
+  void stop();
+}
+
+class Car extends Vehicle {
+  // Implementation of start()
+  @override
+  void start() {
+    print('Car started');
+  }
+
+  // Implementation of stop()
+  @override
+  void stop() {
+    print('Car stopped');
+  }
+}
+
+class Bike extends Vehicle {
+  // Implementation of start()
+  @override
+  void start() {
+    print('Bike started');
+  }
+
+  // Implementation of stop()
+  @override
+  void stop() {
+    print('Bike stopped');
+  }
+}
+
+// void main() {
+//   Car car = Car();
+//   car.start();
+//   car.stop();
+
+//   Bike bike = Bike();
+//   bike.start();
+//   bike.stop();
+// }
+
+// abstract method
+
+abstract class Bank {
+  String name;
+  double rate;
+
+  // Constructor
+  Bank(this.name, this.rate);
+
+  // Abstract method
+  void interest();
+
+  //Non-Abstract method: It have an implementation
+  void display() {
+    print('Bank Name: $name');
+  }
+}
+
+class SBI extends Bank {
+  // Constructor
+  SBI(String name, double rate) : super(name, rate);
+
+  // Implementation of interest()
+  @override
+  void interest() {
+    print('The rate of interest of SBI is $rate');
+  }
+}
+
+class ICICI extends Bank {
+  // Constructor
+  ICICI(String name, double rate) : super(name, rate);
+
+  // Implementation of interest()
+  @override
+  void interest() {
+    print('The rate of interest of ICICI is $rate');
+  }
+}
+
+void main() {
+  SBI sbi = SBI('SBI', 8.4);
+  ICICI icici = ICICI('ICICI', 7.3);
+
+  sbi.interest();
+  icici.interest();
+  icici.display();
+}

7687c92be03ead8e008bb403a550c964f05664d1
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-16 23:24:06 +0530

    polymorph

---
+++ dart-oop/polymorphism.dart
@@ -0,0 +1,20 @@
+class Animal {
+  void eat() {
+    print("Animal is eating");
+  }
+}
+
+class Dog extends Animal {
+  @override
+  void eat() {
+    print("Dog is eating");
+  }
+}
+
+void main() {
+  Animal animal = Animal();
+  animal.eat();
+
+  Dog dog = Dog();
+  dog.eat();
+}

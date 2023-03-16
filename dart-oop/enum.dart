86bb8c4aee1545584db1b9ecedc2b809c31b2b79
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-16 23:47:37 +0530

    enum

---
+++ dart-oop/enum.dart
@@ -0,0 +1,26 @@
+enum Gender { Male, Female, Other }
+
+class Person {
+  // Properties
+  String? firstName;
+  String? lastName;
+  Gender? gender;
+
+  // Constructor
+  Person(this.firstName, this.lastName, this.gender);
+
+  // display() method
+  void display() {
+    print("First Name: $firstName");
+    print("Last Name: $lastName");
+    print("Gender: $gender");
+  }
+}
+
+void main() {
+  Person p1 = Person("John", "Doe", Gender.Male);
+  p1.display();
+
+  Person p2 = Person("Menuka", "Sharma", Gender.Female);
+  p2.display();
+}

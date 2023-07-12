2492eff25f8fbfca0f7400f11816db57e38b3131
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-13 00:53:25 +0530

    app 1 design

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -4,14 +4,22 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+
+    return Scaffold(
       backgroundColor: Colors.black,
       body: ColoredBox(
-        color: Color.fromRGBO(255, 0, 0, 1),
+        color: const Color.fromRGBO(255, 0, 0, 1),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
                 fontSize: 55,
@@ -19,16 +27,25 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                 color: Color.fromARGB(255, 255, 255, 255),
               ),
             ),
-            TextField(
-              style: TextStyle(
-                color: Colors.white,
-              ),
-              decoration: InputDecoration(
-                label: Text(
-                  "Please enter a amount in INR",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                    hintText: 'Please enter the amount in USD',
+                    hintStyle: const TextStyle(
+                      color: Colors.black,
+                    ),
+                    prefixIcon: const Icon(Icons.monetization_on),
+                    prefixIconColor: Colors.black,
+                    filled: true,
+                    fillColor: Colors.white60,
+                    focusedBorder: border,
+                    enabledBorder: border),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
                 ),
               ),
             )

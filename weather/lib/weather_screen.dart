3e8c21ea069fcc3e1d567ae4c2f9be0087f608b9
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-14 23:44:13 +0530

    card designs

--- weather/lib/weather_screen.dart
+++ weather/lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -21,20 +23,65 @@ class WeatherScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Column(
-        children: [
-          const Placeholder(
-            fallbackWidth: 20,
-          ),
-          const SizedBox(height: 20),
-          const Placeholder(
-            fallbackHeight: 150,
-          ),
-          const SizedBox(height: 20),
-          const Placeholder(
-            fallbackHeight: 150,
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Icon(
+                            Icons.cloud,
+                            size: 64,
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            'Rain',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+            const SizedBox(height: 20),
+            const Placeholder(
+              fallbackHeight: 150,
+            )
+          ],
+        ),
       ),
     );
   }
